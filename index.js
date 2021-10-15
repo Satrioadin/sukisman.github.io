3e6221bf772526d365e40804a5326d63902283b4
Satrioadin <satrioadi2525@gmail.com>
2021-10-15 14:58:11 +0700

    inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var tanya = true;
+while (tanya)  {
+    var p = prompt('Siapa nama bapa agung?');
+    var s = 'sukis'
+    
+    // menentukan rules
+    if( p === s){
+        alert('Selamat anda benar!!')
+    }else{
+        alert('Salah goblok, JAWAB YANG BENER!!!')
+    }
+
+    // tampilkan hasil
+    
+
+    // tanya
+    tanya = confirm('Coba lagi ga?');
+
+}
+alert('maaci ya')
